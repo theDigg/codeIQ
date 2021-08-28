a79550e6faaaa4608aabf75daa30a6676c695e1d
Kyle Shifflett <kyle@shardus.com>
2021-08-28 00:25:38 -0700

    Added doc for describing a potentially nice format for testResults from running code

---
+++ docs/challengeSubmittion.js
@@ -0,0 +1,33 @@
+const result = {
+  id: '508',
+  result: {
+    type: 'run',
+    verdict: 'WRONG_ANSWER',
+    testCount: 2,
+    hasErrorOutputs: true,
+    correctTestCount: 0,
+    sampleTestCount: 2,
+    correctSampleTestCount: 0,
+    failedTestNumber: 0,
+    score: 0,
+    maxScore: 300,
+  },
+  testResults: [
+    {
+      userOutput: null,
+      consoleOutput: '1 ',
+      errorOutput: '',
+      correct: false,
+      id: 1,
+      score: 0,
+    },
+    {
+      userOutput: null,
+      consoleOutput: '22 ',
+      errorOutput: '',
+      correct: false,
+      id: 2,
+      score: 0,
+    },
+  ],
+};
